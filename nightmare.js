7881efcafd6b0ac30d151db08db6965fee054093
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-08-18 13:26:47 -0400

    add comments

--- nightmare.js
+++ nightmare.js
@@ -1,3 +1,4 @@
+/* package definitions */
 var csvWriter = require('csv-write-stream');
 var writer = csvWriter();
 var fs = require('fs');
@@ -8,28 +9,35 @@ var Nightmare = require('nightmare');
 	try {
 	nightmare = Nightmare({ show: true });
 	await nightmare
-		.goto('https://www.google.com')
+
+		/* connect to the webpage */
+		.goto('https://www.google.com')		
 		.type('form[action*="/search"] [name=q]', 'datatables')
 		.click('form[action*="/search"] [type=submit]')
-		.wait(2500)
-		await nightmare.evaluate( () => {
-			var link = document.querySelector(".r a");
-			link.click();
-		})
+		.wait()
+
+		/* click on datatables.net link */
+		.click('a[href^="https://datatables.net/"]')
+
 		await nightmare
 			.wait('select[name="example_length"]')
-			.select('select[name="example_length"]', 100);
-		
+			.select('select[name="example_length"]', 100)
+
+		/* extract info from the datatable */
 		await nightmare.evaluate( () => {
 			let tableRows = Array.from(document.querySelectorAll('table#example tr'));
+
+			/* separating values from retrieved table data */
 			let tableKeysRow = Array.from((tableRows[0]).querySelectorAll('th')).map(e => e.innerHTML); 
 			let tableDataRows = tableRows.slice(1,58); 
 			let arrayOfRowObjects = [];
 			let rowObject = {};
 			let formattedValues = [];
 
+			/* extract info from each row */
 			tableDataRows.forEach( row => {
 				formattedValues = Array.from(row.querySelectorAll('td')).map(e => e.innerHTML);
+				
 				rowObject = {};
 				for(i = 0; i < tableKeysRow.length; i++) {
 					rowObject[tableKeysRow[i]] = formattedValues[i];
@@ -39,19 +47,23 @@ var Nightmare = require('nightmare');
 			})
 
 				return arrayOfRowObjects;
-			})
-			.then( result => {
-				writer.pipe(fs.createWriteStream('outputData.csv'));
-				result.forEach( obj => {
-					writer.write(obj);
-				});
-				writer.end();
-			})
-        } catch (error) {
-			console.error(error);
-			throw error;
-		} finally {
-			await nightmare.end();
-		}
+		})
+		.then( result => {
+			console.log("Extracted values from datatable...");
+
+			/* fetch data as a CSV file */
+			writer.pipe(fs.createWriteStream('outputData.csv'));
+			result.forEach( obj => {
+				writer.write(obj);
+			});
+			writer.end();
+			console.log("Wrote values to csv file...")
+		})
+	} catch (error) {
+		console.error("error: ", error);
+		throw error;
+	} finally {
+		await nightmare.end();
+	}
 })();
 
